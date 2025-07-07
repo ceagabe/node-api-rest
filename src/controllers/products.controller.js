9a4cf7054829cdf6703f67969a4cd4d1aaebc49c
ceagabe <gabriel_benedetti@hotmail.com>
2025-07-07 20:38:03 -0300

    agregue la base de dato firestone

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,23 +2,25 @@
 //import * as service from '../services/products.service.js'
 import * as model from '../models/products.model.js'
 
-export const getAllProducts = (req, res)=> {
-    res.json(model.getAllProducts());
+export const getAllProducts = async (req, res)=> {
+    const products = await model.getAllProducts()
+    res.json(products);
 };
 
-export const searchProduct = (req,res)=> {
+
+export const searchProduct = async (req,res)=> {
     console.log(req.query);
     const {name}= req.query;
-    const products =model.getAllProducts(); //agregue para que me traiga el array del model
+    const products = await model.getAllProducts(); //agregue para que me traiga el array del model
     const filteredProducts =products.filter ((p)=> p.name.toLowerCase().includes(name.toLowerCase())
     );
     res.json(filteredProducts); 
 };
 
-export const getProductById= (req, res)=> {
+export const getProductById= async (req, res)=> {
     const {id}=req.params;
     console.log(id);
-    const products =model.getAllProducts();  //agregue para que me traiga el array del model
+    const products =await model.getAllProducts();  //agregue para que me traiga el array del model
     const product=products.find((item)=> item.id == id);
     if(!product){
     res.status(404).json({Error: "No existe el producto"});
@@ -26,31 +28,33 @@ export const getProductById= (req, res)=> {
     res.json(product);
 };
 
-export const createProduct  = (req, res)=> {    
-    const {name, price, categories}= req.body;
-    const products =model.getAllProducts();  //agregue para que me traiga el array del model
-    const newProduct= model.createProduct({name,price,categories});
+export const createProduct  = async (req, res)=> {    
+    const {name, price, category, stock}= req.body;
+    const products =await model.getAllProducts();  //agregue para que me traiga el array del model
+    const newProduct=await model.createProduct({name,price,category,stock});
     res.status(201).json(newProduct);
+    console.log(newProduct);
 };
 
 
 
-export const updateProduct =(req,res)=>{
-    const products =model.getAllProducts();  //agregue para que me traiga el array del model
+
+export const updateProduct =async (req,res)=>{
+    const products =await model.getAllProducts();  //agregue para que me traiga el array del model
     const productId =parseInt(req.params.id, 10);
     const productIndex= products.findIndex((item)=>item.id === productId);
     if(productIndex=== -1){
         return res.status(404).json({error: "Producto no encontrado"});
     }
-    const {name, price}= req.body;
-    products[productIndex]= { id:productId,name,price};
+    const {name, price, category, stock}= req.body;
+    products[productIndex]= { id:productId,name,price,category,stock};
     res.json(products[productIndex]);
     
 };
 
-export const deleteProduct= (req,res)=>{
+export const deleteProduct= async (req,res)=>{
     const productId =parseInt(req.params.id, 10);  // base 10
-    const product= model.deleteProduct(productId);
+    const product=await model.deleteProduct(productId);
    
     if(!product){
         return res.status(404).json({error: "Producto no encontrado, el producto fue borrado"});
--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,46 +1,100 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 //import { createProduct } from '../controllers/products.controller';
 
-const __dirname =import.meta.dirname;
-const dataPath = path.join(__dirname, './products.json');
-const json= fs.readFileSync(dataPath,'utf-8');
-const products= JSON.parse(json);   //esto es un objeto en js
+import { db } from '../data/data.js'; 
+  import { 
+      collection, 
+      getDocs, 
+      getDoc, 
+      addDoc, 
+      deleteDoc, 
+      doc,
+      query
+  } from 'firebase/firestore'; 
+ 
+const productsCollection = collection(db, 'products'); 
 
-console.log(products);
+  
+
+// const __dirname =import.meta.dirname;
+// const dataPath = path.join(__dirname, './products.json');
+// const json= fs.readFileSync(dataPath,'utf-8');
+//const products= JSON.parse(json);   //esto es un objeto en js
 
 
-export const getAllProducts = ()=> {
-    return products;
-};
+ export async function getProductById(id) { 
+    const productDoc = await getDoc(doc(productsCollection, id)); 
+    if (productDoc.exists()) { 
+       
+        return productDoc.data(); 
 
-export const getAllProductsbyId= (id) =>{
-    return products.find((item)=>item.id == id);
-};
+    } else { 
+        return null; 
+    } 
+  }; 
+
+ 
+  // Método para obtener todos los productos 
+  export async function getAllProducts() {  
+    
+    const querySnapshot = await getDocs(productsCollection); 
+    const products = []; 
+    querySnapshot.forEach((doc) => { 
+        products.push({ id: doc.id, ...doc.data() }); 
+    }); 
+    return products; 
+  }; 
+ 
+  // Método para guardar un producto en Firestore -----cambien save por create
+  export async function createProduct(product,id) { 
+    await addDoc(productsCollection, product); 
+  }; 
+ 
+  export async function updateProduct(product) { 
+    await addDoc(productsCollection, product); 
+  }; 
+
+  // Método para eliminar un producto por su ID 
+  export async function deleteProduct(id) { 
+    await deleteDoc(doc(productsCollection, id)); 
+
+  };
+
+// console.log(products);
 
 
-export const createProduct  = (data)=> {       
-    const newProduct={
-        id:products.length + 1, 
-        ...data, 
-    };
-    products.push(newProduct);
-    //res.status(201).json(newProduct);
-    fs.writeFileSync(dataPath, JSON.stringify(products));
-    console.log(newProduct);
-    return newProduct
+// export const getAllProducts = ()=> {
+//     return products;
+// };
 
-};
+// export const getAllProductsbyId= (id) =>{
+//     return products.find((item)=>item.id == id);
+// };
 
-export const deleteProduct=(id)=>{
-    const productIndex= products.findIndex((item)=>item.id === id);
-    if (productIndex == -1 ){
-        return null;
-    }else{
-    const  product = products.splice(productIndex,1);  //elimina un elemento
-    fs.writeFileSync(dataPath, JSON.stringify(products));
-     return products
-    };
+
+// export const createProduct  = (data)=> {       
+//     const newProduct={
+//         id:products.length + 1, 
+//         ...data, 
+//     };
+//     products.push(newProduct);
+//     //res.status(201).json(newProduct);
+//     fs.writeFileSync(dataPath, JSON.stringify(products));
+//     console.log(newProduct);
+//     return newProduct
+
+// };
+
+// export const deleteProduct=(id)=>{
+//     const productIndex= products.findIndex((item)=>item.id === id);
+//     if (productIndex == -1 ){
+//         return null;
+//     }else{
+//     const  product = products.splice(productIndex,1);  //elimina un elemento
+//     fs.writeFileSync(dataPath, JSON.stringify(products));
+//      return products
+//     };
    
-    //console.log(products);
-};
+//     //console.log(products);
+// };
